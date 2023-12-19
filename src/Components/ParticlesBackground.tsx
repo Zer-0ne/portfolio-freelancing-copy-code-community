350b348560edf5ac044e769bc2c1ea9a7299d651
Zer-0ne <eagle@kali.eagle>
2023-12-19 15:20:35 +0530

    add in home page

--- src/Components/ParticlesBackground.tsx
+++ src/Components/ParticlesBackground.tsx
@@ -1,9 +1,10 @@
 'use client'
 import Image from 'next/image';
 import React from 'react'
-// import Particles from 'react-particles'
-// import particleConfig from './Config/particles.config'
+import Particles from 'react-particles'
+import particleConfig from './Config/particles.config'
 import { loadFull } from 'tsparticles'
+import bg from '@/assests/1.gif'
 import type { Engine, ISourceOptions } from "tsparticles-engine";
 
 const ParticlesBackground = (
@@ -13,48 +14,12 @@ const ParticlesBackground = (
     await loadFull(engine);
   };
   return (
-    // <Particles
-    //   id="tsparticles"
-    //   options={particleConfig as ISourceOptions}
-    //   init={customInit}
-    // />
-    <>
-      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }}>
-        {/* Replace 'your_video.mp4' with the path or URL of your video */}
-        <video autoPlay muted loop style={{ width: '100%', height: '100%' }}>
-          <source src={`https://drive.google.com/file/d/11UHvcP48uIwkUOMrodrxbb639-hd9P13/view?usp=sharing`} type="video/mp4" />
-        </video>
-      </div>
-      <style jsx>{`
-        @media screen and (max-width: 600px) {
-          video {
-            transform: scale(3.5); // Adjust the zoom level as needed
-          }
-        }
-        @media screen and (max-width: 1200px) {
-          video {
-            transform: scale(5.5); // Adjust the zoom level as needed
-          }
-        }
-        @media screen and (min-width: 1200px) {
-          video {
-            transform: scale(1.5); // Adjust the zoom level as needed
-          }
-        }
-      `}</style>
-      {/* <video
-        preload="auto"
-        playsInline
-        autoPlay
-        muted
-        loop
-        style={{ zIndex: -1, position: 'absolute', inset: 0 }}
-      >
-        <source
-          src={`https://firebasestorage.googleapis.com/v0/b/copycodecommunity-a082f.appspot.com/o/bg%2FbgVideo.mp4?alt=media&token=013cb9fc-0fc3-4aaa-bea9-18f7a522a3c7`}
-        /> */}
-      {/* </video> */}
-    </>
+    <Particles
+      id="tsparticles"
+      options={particleConfig as ISourceOptions}
+      init={customInit}
+    />
+    
   )
 }
 
