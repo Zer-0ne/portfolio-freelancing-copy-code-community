8b9d1f70c7a1fbc26815565d99b0d0b5b402aa1b
Zer-0ne <eagle@kali.eagle>
2023-12-20 20:13:50 +0530

    add delete the image from the firebase and resolve the bugs

--- src/utils/constant.tsx
+++ src/utils/constant.tsx
@@ -408,7 +408,7 @@ export const coreTeamMember: coreMember[] = [
 //     },
 // ]
 
-export const eventsDetails: Data[] = [
+export const eventsDetails: EventsInterface[] = [
     {
         title: 'React Native Workshop',
         description: 'This is the tamplate description of React native workshop',
@@ -416,10 +416,12 @@ export const eventsDetails: Data[] = [
         calenderDate: 'Oct 24, 2023',
         tag: 'React_native',
         mode: 'Online',
-        participants: '146',
+        participants: 146,
         status: 'End',
         image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.filepicker.io%2Fapi%2Ffile%2F4C6yPDywSUeWYLyg1h9G&f=1&nofb=1&ipt=be055fef91c6c9eb73b7c305ad18a9e55e560b114fa34d2519ce26a985bedee7&ipo=images',
         label: 'Featured',
+        _id: 'sahi',
+        contentImage:['']
     },
     {
         title: 'React Workshop',
@@ -428,10 +430,12 @@ export const eventsDetails: Data[] = [
         calenderDate: 'Dec 24, 2023',
         tag: 'React',
         mode: 'Offline',
-        participants: '155',
+        participants: 155,
         status: 'End',
         image: 'https://d112y698adiu2z.cloudfront.net/photos/production/challenge_thumbnails/002/610/587/datas/medium_square.png',
         label: 'UpComming',
+        _id: 'sas',
+        contentImage:['']
     },
     {
         title: 'Node Workshop',
@@ -440,9 +444,11 @@ export const eventsDetails: Data[] = [
         calenderDate: 'Nov 24, 2023',
         tag: 'Node.js',
         mode: 'Offline',
-        participants: '200',
+        participants: 200,
         status: 'End',
         image: 'https://d112y698adiu2z.cloudfront.net/photos/production/challenge_thumbnails/002/610/587/datas/medium_square.png',
+        _id: 'shi',
+        contentImage:['']
     },
     {
         title: 'Next Workshop',
@@ -451,10 +457,12 @@ export const eventsDetails: Data[] = [
         calenderDate: 'Dec 31, 2024',
         tag: 'Next.js',
         mode: 'Hybrid',
-        participants: '500',
+        participants: 500,
         status: 'Upcoming',
         image: 'https://d112y698adiu2z.cloudfront.net/photos/production/challenge_thumbnails/002/610/587/datas/medium_square.png',
         label: 'Featured',
+        _id: 'nami',
+        contentImage:['']
     },
 ]
 
