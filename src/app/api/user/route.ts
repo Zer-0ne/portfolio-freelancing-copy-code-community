1c07a14e9f6f08c75982948fedc21e66e5e9d53b
Zer-0ne <eagle@kali.eagle>
2024-01-04 21:17:46 +0530

    protect the user get route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,5 +1,7 @@
 
 import Users from "@/Models/Users";
+import { Session } from "@/utils/Interfaces";
+import { currentSession } from "@/utils/Session";
 import connect from "@/utils/database";
 import { NextResponse } from "next/server";
 
@@ -7,6 +9,14 @@ import { NextResponse } from "next/server";
 export const GET = async () => {
     await connect();
     const users = await Users.find({});
+
+    const session = await currentSession() as Session;
+    if (!session) return NextResponse.json({ message: 'Please login' }, { status: 401 })
+
+    // check the user is admin or not 
+    const user = await Users.findOne({ username: session?.user?.username })
+    if (['user','moderator'].includes(user.role)) return NextResponse.json({ message: 'You are not Authorized!' }, { status: 401 })
+
 
     // Create a new array without the password field
     const sanitizedUsers = users.map(user => {
