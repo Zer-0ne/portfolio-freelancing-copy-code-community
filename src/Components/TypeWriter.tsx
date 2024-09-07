dd59321bb5b4d7f2f54aba1e85c9ee522abf5749
Zer-0ne <sahilkhan8294799@gmail.com>
2024-09-07 16:30:15 +0530

    added typewriter features in home screen and tranisitions

---
+++ src/Components/TypeWriter.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
+const Typewriter = ({ texts, speed, onFinish, className, ref, style }: {
+    texts: string[];
+    speed: number;
+    className?: string
+    onFinish: () => void,
+    ref?: any;
+    style?: any
+}) => {
+    const [displayedText, setDisplayedText] = useState("");
+    const [textIndex, setTextIndex] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [isFinished, setIsFinished] = useState(false);
+
+    useEffect(() => {
+        const currentText = texts[textIndex];
+
+        const timeout = setTimeout(() => {
+            if (isFinished) return; // Stop if finished
+
+            if (isDeleting) {
+                setDisplayedText(currentText.substring(0, displayedText.length - 1));
+                if (displayedText === "") {
+                    // Wait for 1-2 seconds before moving to the next text
+                    if (textIndex === texts.length - 1) {
+                        setIsFinished(true); // Mark as finished if it's the last text
+                        onFinish(); // Call the onFinish callback
+                    } else {
+                        setIsDeleting(false);
+                        setTextIndex((prevIndex) => prevIndex + 1); // Move to the next text
+                    }
+                }
+            } else {
+                setDisplayedText(currentText.substring(0, displayedText.length + 1));
+                if (displayedText === currentText) {
+                    // Wait for 1-2 seconds before stopping
+                    if (textIndex === texts.length - 1) {
+                        setIsFinished(true); // Mark as finished if it's the last text
+                        onFinish(); // Call the onFinish callback
+                    } else {
+                        // Wait before starting to delete
+                        setTimeout(() => {
+                            setIsDeleting(true);
+                        }, 1000); // Adjust this value for the pause duration
+                    }
+                }
+            }
+        }, isDeleting ? speed / 3 : speed); // Deleting is 3 times faster
+
+        return () => clearTimeout(timeout);
+    }, [displayedText, isDeleting, speed, textIndex, texts, isFinished, onFinish]);
+
+    return (
+        <Typography
+            variant='body1'
+            sx={{
+                fontSize: { xl: 20, md: 16, xs: 14 },
+            }} style={style} className={`${className}`}>
+            {displayedText}
+        </Typography>
+    );
+};
+
+export default Typewriter;
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,8 +1,9 @@
 import { Box, Typography } from '@mui/material'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { styles } from '@/utils/styles'
 import { Rubik_Glitch, IBM_Plex_Mono, Satisfy } from 'next/font/google'
 import Link from 'next/link';
+import Typewriter from '@/Components/TypeWriter';
 
 // Load the font using the font loader
 const rubikGlitchFont = Rubik_Glitch({
@@ -19,7 +20,8 @@ const smooch = Satisfy({
 })
 
 const HomePage = () => {
-
+    const [isFinish, setIsFinish] = useState(false)
+    const typewriterRef = useRef(null); // Separate ref for Typewriter
 
     return (
         <Box
@@ -30,6 +32,7 @@ const HomePage = () => {
                 flexDirection: 'column',
                 height: '100vh',
                 maxHeight: '100vh',
+                transition: 'all .10s ease-in-out',
                 flex: 1,
                 // backgroundColor: '#283593',
             }}
@@ -62,6 +65,9 @@ const HomePage = () => {
                         fontWeight: 800,
                         fontSize: { xs: '2rem', md: '4rem', xl: '6rem' },
                         textAlign: 'center',
+                        // transform: isFinish ? 'scale(1)' : 'scale(0)',
+                        // opacity:isFinish?'1':'0',
+                        // transition: 'all 1.9s ease-in-out',
                         // background: 'linear-gradient(to right, red, green,red)',
                         // backgroundClip: 'text',
                         // WebkitTextFillColor: 'transparent',
@@ -81,6 +87,9 @@ const HomePage = () => {
                 <Typography variant='body1'
                     sx={{
                         color: 'green',
+                        // transform: isFinish ? 'scale(1)' : 'scale(0)',
+                        // // opacity:isFinish?'1':'0',
+                        // transition: 'all 1.9s ease-in-out',
                         textTransform: 'uppercase',
                         fontSize: { xl: 25, md: 16, xs: 10 }
                     }}
@@ -91,10 +100,23 @@ const HomePage = () => {
                 sx={{
                     width: { xl: '40%', xs: '70%', md: '40%' },
                     textAlign: 'center',
-                    marginTop: 3
+                    marginTop: 2,
+                    minHeight: !isFinish ? { md: '120px', xs: '150px' } : 'auto'
                 }}
             >
-                <Typography
+                <Typewriter
+                    ref={typewriterRef} // Use separate ref for Typewriter
+                    texts={[
+                        "Welcome to the Copy Code Community, a vibrant hub where coding enthusiasts, web developers, and tech aficionados unite! Here, we celebrate the art of programming and the joy of creating innovative solutions together.",
+                        "Whether you're a seasoned developer or just embarking on your coding journey, our community is a welcoming space for everyone. Share your code, seek advice, and engage in lively discussions about the latest trends in technology and development.",
+                        "In this collaborative environment, you'll find opportunities to connect with like-minded individuals, explore diverse projects, and enhance your skills. From beginner tutorials to advanced coding challenges, there's something for everyone to learn and grow.",
+                        "Join us as we embark on exciting coding adventures, collaborate on projects that inspire, and foster a culture of creativity and innovation. Together, we can build amazing things and make a positive impact in the tech world!"
+                    ]}
+                    className={`${smooch.className}`}
+                    speed={10}
+                    onFinish={() => { setIsFinish(true) }}
+                />
+                {/* <Typography
                     variant='body1'
                     sx={{
                         fontSize: { xl: 20, md: 16, xs: 14 },
@@ -103,19 +125,24 @@ const HomePage = () => {
 
                 >
                     Copy Code Community is a friendly space where people who love coding, web development, and new tech come together. Whether you're a pro or just starting out, everyone is welcome to share code, discuss cool tech stuff, and learn together. It's a place to connect with others, explore the world of software, and have fun while building cool things!
-                </Typography>
+                </Typography> */}
             </Box>
-            <Link
-                href='/materials'
-                style={{
-                    marginTop: 30,
-                    padding: '10px 15px',
-                    backgroundColor: 'rgba(0, 0, 0, .7)',
-                    borderRadius: 5,
-                    border: '1px solid rgba(255,255,255,.5)'
-                }}
-                className={ibn.className}
-            >Start Learning...</Link>
+            {
+                <Link
+                    href='/materials'
+                    style={{
+                        // transform: !isFinish ? 'skew(180deg,10deg)' : 'skew(0deg,0deg)',
+                        transform: isFinish ? 'scale(1)' : 'scale(0)',
+                        transition: 'all .5s ease-in-out',
+                        marginTop: 15,
+                        padding: '10px 15px',
+                        backgroundColor: 'rgba(0, 0, 0, .7)',
+                        borderRadius: 5,
+                        border: '1px solid rgba(255,255,255,.5)',
+                        fontSize: '.8rem'
+                    }}
+                    className={ibn.className}
+                >Start Learning...</Link>}
         </Box >
     )
 }
